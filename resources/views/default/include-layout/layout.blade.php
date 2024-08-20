0565654412ada9d12b9c4b6fabbdfab3a3327484
IAify <iatify.team@gmail.com>
2024-08-20 13:26:21 +0500

    development Your branch is up to date with 'origin/development'.
    Changes to be committed:
    	modified:   resources/views/default/include-layout/layout.blade.php

--- resources/views/default/include-layout/layout.blade.php
+++ resources/views/default/include-layout/layout.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ url('public/assets/css/bootstrap.css') }}">
-    <link rel="stylesheet" href="{{ url('public/assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ url('public/assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/main.css') }}">
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
         integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
