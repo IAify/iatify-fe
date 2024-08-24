1b10273f48de33a2a091971a915e88ae6a3a01ce
Hamza Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2024-08-24 05:58:47 +0500

    image issue fix

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,6 +3,6 @@
 if (! function_exists('lang_route')) {
     function lang_route(string $name, array $parameters = [], bool $absolute = true)
     {
-        return route($name, array_merge($parameters, ['locale' => app()->getLocale()]), $absolute);
+        return route($name, array_merge($parameters, ['locale' => '']), $absolute);
     }
 }
